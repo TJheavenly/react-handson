4cfbe926af3482fca661f37f943e45a9da223dd6
TJheavenly <takajun5252@gmail.com>
2022-04-10 23:17:18 +0900

    画面表示ができるように修正

--- state-management/src/StarRating.js
+++ state-management/src/StarRating.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { FaSter } from "react-icons/fa"
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa"
 
 const Star = ({ selected = false }) => (
-    <FaSter color={selected ? "red" : "gray"} />
+    <FaStar color={selected ? "red" : "gray"} />
 );
 
 export default function StarRating({ totalStars = 5 }) {
     const [selectedStars] = useState(3);
     return (
         <>
-            {[...Array(totalStars)].map((_, i) => (
+            {[...Array(totalStars)].map((n, i) => (
                 <Star key={i} selected={selectedStars > i} />
             ))}
             <p>
