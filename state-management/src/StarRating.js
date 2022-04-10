1cca1c5382bf0b55a8092b8ea51e87060c836cf7
TJheavenly <takajun5252@gmail.com>
2022-04-10 23:44:39 +0900

    星を選択できる処理を追加

--- state-management/src/StarRating.js
+++ state-management/src/StarRating.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
-import { FaStar } from "react-icons/fa"
+import Star from "./Star";
 
-const Star = ({ selected = false }) => (
-    <FaStar color={selected ? "red" : "gray"} />
-);
-
-export default function StarRating({ totalStars = 5 }) {
-    const [selectedStars] = useState(3);
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
+    const [selectedStars, setSelectedStars] = useState(0);
     return (
-        <>
+        <div style={{ padding: 5, ...style }} {...props}>
             {[...Array(totalStars)].map((n, i) => (
-                <Star key={i} selected={selectedStars > i} />
+                <Star 
+                    key={i} 
+                    selected={selectedStars > i}
+                    onSelect={() => setSelectedStars(i + 1)} 
+                />
             ))}
             <p>
                 {selectedStars} of {totalStars} stars
             </p>
-        </>
+        </div>
     );
 }
